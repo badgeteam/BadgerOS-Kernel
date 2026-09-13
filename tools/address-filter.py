425b98d447c81abb7a27580b44a5c594006c868e
Julian Scheffers <julian@scheffers.net>
2026-09-13 13:08:18 +0200

    Make the address filter much faster

--- tools/address-filter.py
+++ tools/address-filter.py
@@ -2,7 +2,7 @@
 
 # SPDX-License-Identifier: MIT
 
-import sys, subprocess, os
+import sys, subprocess, os, signal
 from argparse import *
 
 assert __name__ == "__main__"
@@ -19,23 +19,23 @@ hexits = 16 if args.long else 8
 
 state = -2
 
-prefix = os.path.realpath('.') or ""
+prefix = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+addr2line_process = subprocess.Popen([args.addr2line, '-e', args.file], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 def addr2line():
-    global buf
-    cmd = [
-        args.addr2line, '-e', args.file, buf
-    ]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    result  = process.stdout.read()
-    process.wait()
-    string  = result.decode().strip()
-    if process.returncode: return
+    global buf, addr2line_process, prefix
+    addr2line_process.stdin.write(buf.encode())
+    addr2line_process.stdin.write(b'\n')
+    addr2line_process.stdin.flush()
+    string = addr2line_process.stdout.readline().decode().strip()
     if string == '??:0': return
     if string.endswith('?'): return
     sys.stdout.write(" (")
-    if string.startswith(prefix):
-        sys.stdout.write(os.path.relpath(string))
+    if "/rustlib/src/" in string:
+        sys.stdout.write(string[string.index("/rustlib/")+9:])
+    elif string.startswith(prefix):
+        sys.stdout.write(os.path.relpath(string, prefix))
     else:
         sys.stdout.write(string)
     sys.stdout.write(")")
@@ -44,7 +44,11 @@ while True:
     try:
         msg_raw = sys.stdin.buffer.read(1)
     except (KeyboardInterrupt, EOFError):
+        os.kill(addr2line_process.pid, signal.SIGINT)
         break
+    except Exception as e:
+        os.kill(addr2line_process.pid, signal.SIGINT)
+        raise e
     if not len(msg_raw): break
     msg = chr(msg_raw[0])
     for char in msg:
